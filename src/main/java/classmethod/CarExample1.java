60acd221474affd8cbfeee58672a76e25d329f15
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-05 01:07:18 +0900

    생성자 선언, 객체 생성

---
+++ src/main/java/classmethod/CarExample1.java
@@ -0,0 +1,11 @@
+package classmethod;
+
+public class CarExample1 {
+
+	public static void main(String[] args) {
+		Car1 myCar = new Car1("검정", 3000);
+		//Car1 myCar = new Car1(); (X) -> 기본 생성자 호출할 수 없다.
+		
+	}
+
+}
