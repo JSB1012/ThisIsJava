afd3f7e8c4ca8aa8577428cfc975be08163403d9
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-05 15:55:09 +0900

    정적 멤버 사용

---
+++ src/main/java/classmethod2/Calculator1.java
@@ -0,0 +1,14 @@
+package classmethod2;
+
+public class Calculator1 {
+	static double pi = 3.14159;
+
+	static int plus(int x, int y) {
+		return x + y;
+	}
+
+	static int minus(int x, int y) {
+		return x - y;
+	}
+
+}
