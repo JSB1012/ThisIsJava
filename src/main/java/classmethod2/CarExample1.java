7328e524ed39067479b0dc267eaef4f0321d16f4
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-05 15:48:34 +0900

    인스턴스 멤버와 this

---
+++ src/main/java/classmethod2/CarExample1.java
@@ -0,0 +1,14 @@
+package classmethod2;
+
+public class CarExample1 {
+
+	public static void main(String[] args) {
+		Car1 myCar = new Car1("포르쉐");
+		Car1 yourCar = new Car1("벤츠");
+
+		myCar.run();
+		yourCar.run();
+
+	}
+
+}
