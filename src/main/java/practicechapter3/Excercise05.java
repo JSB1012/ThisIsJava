de9d732df242efe368cbd46b53a9dc7e24d22ac2
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-08 13:19:39 +0900

    확인문제 chapter3

---
+++ src/main/java/practicechapter3/Excercise05.java
@@ -0,0 +1,11 @@
+package practicechapter3;
+
+public class Excercise05 {
+
+	public static void main(String[] args) {
+		int value = 356;
+		System.out.println(356 / 100 * 100);
+
+	}
+
+}
---
+++ src/main/java/practicechapter3/Excercise06.java
@@ -0,0 +1,14 @@
+package practicechapter3;
+
+public class Excercise06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (lengthTop + lengthBottom) * height / 2.0;
+		System.out.println(area);
+
+	}
+
+}
