4174c14bd10cbefec820a51e9b5f48de7a4012eb
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-08 16:06:47 +0900

    확인문제 chapter4

---
+++ src/main/java/practicechpater4/Excercise06.java
@@ -0,0 +1,17 @@
+package practicechpater4;
+
+public class Excercise06 {
+
+	public static void main(String[] args) {
+		for (int x = 1; x <= 5; x++) {
+			for (int y = 1; y <= x; y++) {
+				System.out.print("*");
+				if (y == x) {
+					System.out.println();
+				}
+			}
+		}
+
+	}
+
+}
---
+++ src/main/java/practicechpater4/Excercise07.java
@@ -0,0 +1,46 @@
+package practicechpater4;
+
+import java.util.Scanner;
+
+public class Excercise07 {
+
+	public static void main(String[] args) {
+		boolean run = true;
+
+		int balance = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (run) {
+			System.out.println("----------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔금 | 4.종료");
+			System.out.println("----------------------------------");
+			System.out.print("선택> ");
+
+			int a = scanner.nextInt();
+
+			switch (a) {
+			case 1:
+				System.out.print("예금액>");
+				balance += scanner.nextInt();
+				break;
+			case 2:
+				System.out.print("출금액>");
+				balance -= scanner.nextInt();
+				break;
+			case 3:
+				System.out.println(balance);
+				break;
+			case 4:
+				run = false;
+				break;
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("프로그램 종료");
+
+	}
+
+}
