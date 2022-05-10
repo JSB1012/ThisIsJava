d68f4d638d5a9291b713d12bc49fea8b1778025d
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-11 00:43:52 +0900

    인터페이스 배열로 필드 다형성

---
+++ src/main/java/interface2/Car2.java
@@ -0,0 +1,16 @@
+package interface2;
+
+public class Car2 {
+	Tire[] tires = {
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire()
+	};
+
+	void run() {
+		for(Tire tire : tires) {
+			tire.roll();
+		}
+	}
+}
