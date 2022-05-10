f4924e62318018ed8dd77bb1e422fcd4f7c73413
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-11 00:51:05 +0900

    매개 변수의 인터페이스화

---
+++ src/main/java/interface2/Driver.java
@@ -0,0 +1,8 @@
+package interface2;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+
+}
---
+++ src/main/java/interface2/DriverExample.java
@@ -0,0 +1,16 @@
+package interface2;
+
+public class DriverExample {
+
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+
+		driver.drive(bus); // 자동타입 변환 : Vehicle vehicle = bus;
+		driver.drive(taxi); // 자동타입 변환 : Vehicle vehicle = taxi;
+
+	}
+
+}
