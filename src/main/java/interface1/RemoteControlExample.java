42026e4f51f65c73d927d82dd5acd5e6fb4be266
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-10 23:39:41 +0900

    인터페이스 변수에 구현 객체 대입

---
+++ src/main/java/interface1/RemoteControlExample.java
@@ -0,0 +1,12 @@
+package interface1;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl4 rc;
+		rc = new Television();
+		rc = new Audio();
+
+	}
+
+}
