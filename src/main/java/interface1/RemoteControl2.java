36d0c313eb6ab77958b12e6474ae6f19b66768d6
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-10 23:21:49 +0900

    메소드 선언

---
+++ src/main/java/interface1/RemoteControl2.java
@@ -0,0 +1,15 @@
+package interface1;
+
+public interface RemoteControl2 {
+	// 상수
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+
+	// 추상 메소드
+	public void turnOn();
+
+	public void turnOff();				// 메소드 선언부만 작성(추상 메소드)
+
+	public void setVolume(int volume);
+
+}
