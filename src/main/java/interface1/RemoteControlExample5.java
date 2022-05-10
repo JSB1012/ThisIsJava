6256690b3bf4d0d7a647849247ab6ca262bc298e
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-11 00:20:14 +0900

    정적 메소드 사용

---
+++ src/main/java/interface1/RemoteControlExample5.java
@@ -0,0 +1,10 @@
+package interface1;
+
+public class RemoteControlExample5 {
+
+	public static void main(String[] args) {
+		RemoteControl4.changeBattery(); // 인터페이스의 정적 메소드는 인터페이스로 바로 호출이 가능
+
+	}
+
+}
