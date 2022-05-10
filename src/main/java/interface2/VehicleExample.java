9222fd33eed2a3182ab3aaf9eb6b698102fc5c4e
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-11 00:57:51 +0900

    강제 타입 변환

---
+++ src/main/java/interface2/VehicleExample.java
@@ -0,0 +1,18 @@
+package interface2;
+
+public class VehicleExample {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Bus();
+
+		vehicle.run();
+		// vehicle.checkFare(); X // Vehicle 인터페이스에는 checkFare() 없음
+
+		Bus bus = (Bus) vehicle; // 강제 타입 변환
+
+		bus.run();
+		bus.checkFare(); // Bus 클래스에는 checkFare()가 있음
+
+	}
+
+}
