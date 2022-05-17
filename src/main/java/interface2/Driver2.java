9183c394219e7ae39e2fe51e1647dd9b8e42a8d4
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-17 22:00:25 +0900

    instanceof

---
+++ src/main/java/interface2/Driver2.java
@@ -0,0 +1,12 @@
+package interface2;
+
+public class Driver2 {
+	public void drive(Vehicle vehicle) {
+		if(vehicle instanceof Bus) {
+			Bus bus = (Bus) vehicle;
+			bus.checkFare();
+		}
+		vehicle.run();
+	}
+
+}
