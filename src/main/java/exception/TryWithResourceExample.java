492f43da9f85551f38cc8a3b8fed200d20d04a02
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-24 23:27:47 +0900

    AutoCloseable 구현 클래스

---
+++ src/main/java/exception/TryWithResourceExample.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class TryWithResourceExample {
+
+	public static void main(String[] args) {
+		try (FileInputStream fis = new FileInputStream("file.txt")) {
+			fis.read();
+			throw new Exception(); // 강제적으로 예외발생시킴
+		} catch (Exception e) {
+			System.out.println("예외 처리 코드가 실행되었습니다.");
+		}
+
+	}
+
+}
