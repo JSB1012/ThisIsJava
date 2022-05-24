18e94f2b8823442bdfb7a58bf3a47015b08548a1
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-24 22:47:33 +0900

    NullPointerException

---
+++ src/main/java/exception/NullPounterExceptionExample.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class NullPounterExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null; //String 객체 참조 x
+		System.out.println(data.toString());
+
+	}
+
+}
