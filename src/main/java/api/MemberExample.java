167c33baba90326bf4de6ec6bc48cc23e34d9bfd
HODU <HODU@DESKTOP-D1E9QIM>
2022-05-28 12:13:07 +0900

    객체 동등 비교(equals() 메소드)

---
+++ src/main/java/api/MemberExample.java
@@ -0,0 +1,16 @@
+package api;
+
+public class MemberExample {
+	public static void main (String[] args) {
+		Member obj1 = new Member("blue");
+		Member obj2 = new Member("blue");
+		Member obj3 = new Member("red");
+		
+		if(obj1.equals(obj2)) {
+			System.out.println("obj1과 obj2는 동등합니다.");
+		} else {
+			System.out.println("obj1과 obj3은 동등하지 않습니다.");
+		}
+	}
+
+}
